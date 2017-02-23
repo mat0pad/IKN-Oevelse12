09b9d5d9b808c2b52e6c5b80871a6583d3bf1dfe
root <antonzxcz@yahoo.dk>
2017-02-23 21:16:30 +0100

    added comments to client

--- file_client/file_client.cs
+++ file_client/file_client.cs
@@ -8,22 +8,13 @@ namespace tcp
 	class file_client
 	{
 		TcpClient clientSocket = new TcpClient();
+
 		NetworkStream serverStream;
-		/// <summary>
-		/// The PORT.
-		/// </summary>
+
 		const int PORT = 9000;
-		/// <summary>
-		/// The BUFSIZE.
-		/// </summary>
+
 		const int BUFSIZE = 1000;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="file_client"/> class.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments. First ip-adress of the server. Second the filename
-		/// </param>
 		private file_client (string[] args)
 		{
 			
@@ -35,9 +26,7 @@ namespace tcp
 			receiveFile (Request, serverStream);
 
 		}
-
-
-
+			
 		private string SendRequest()
 		{
 			Console.WriteLine ("Write the name of file:");
@@ -53,15 +42,7 @@ namespace tcp
 			Console.WriteLine ("Size is: {0}", size);
 			return size;
 		}
-		/// <summary>
-		/// Receives the file.
-		/// </summary>
-		/// <param name='fileName'>
-		/// File name.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for reading from the server
-		/// </param>
+
 		private void receiveFile (String fileName, NetworkStream io)
 		{
 			
@@ -93,15 +74,8 @@ namespace tcp
 			Console.WriteLine ("File recieved");
 			File.WriteAllBytes("/root/Desktop/"+fileName, data);
 			Console.WriteLine (fileName + " Saved on Desktop");
-			// TO DO Your own code
 		}
-
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
+			
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Client starts...");
--- file_server/file_server.cs
+++ file_server/file_server.cs
@@ -34,64 +34,55 @@ namespace tcp
 			TcpClient clientSocket = default(TcpClient);
 
 			Console.WriteLine (" >> Server Started");
-			serverSocket.Start ();
+			serverSocket.Start (); //Starts serversocket
 
 
 
 			//int requestCount = 0;
 
 			while (true) {
-				clientSocket = serverSocket.AcceptTcpClient ();
-				Console.WriteLine (" >> Accept connection from client");
-				NetworkStream networkStream = clientSocket.GetStream ();
+				clientSocket = serverSocket.AcceptTcpClient (); //Accept connection with client
+				Console.WriteLine (" >> Accept connection from client"); 
+				NetworkStream networkStream = clientSocket.GetStream (); //Network stream for sending and recieving data
 				long size = 0;
 				do
 				{
-
-
-					fileName = LIB.readTextTCP (networkStream);
+					fileName = LIB.readTextTCP (networkStream); //reads filename from client
 					size = LIB.check_File_Exists (fileName); //checks if file exist
-					LIB.writeTextTCP (networkStream,size.ToString());
-				}while(size == 0);
+					LIB.writeTextTCP (networkStream,size.ToString()); //Send size to client
+				}while(size == 0); //Check if file exist 
 				sendFile (fileName, size, networkStream);
-
+				clientSocket.Close (); //Disconnet tcp connection
+				Console.WriteLine("Connection closed with client");
 			}
 		}
 		private void sendFile (String fileName, long fileSize, NetworkStream io)
 		{
 			Console.WriteLine ("Sending file...");
-			byte[] data = File.ReadAllBytes (fileName); //reading data
+			byte[] data = File.ReadAllBytes (fileName); //Saves file content on data 
 
-			byte[] dataLength = BitConverter.GetBytes (data.Length);
-			byte[] package = new byte[4 + data.Length];
-			dataLength.CopyTo (package, 0);
-			data.CopyTo (package, 4);
+			byte[] dataLength = BitConverter.GetBytes (data.Length); //
+			byte[] package = new byte[4 + data.Length]; //prepares package for send
+			dataLength.CopyTo (package, 0); //copies datalength to package
+			data.CopyTo (package, 4); //copies data to package
 
 			int bytesSent = 0;
 			int bytesLeft = package.Length;
 
-			while (bytesLeft > 0)
+			while (bytesLeft > 0) //keeps going until all bytes are send
 			{
 
 				int nextPacketSize = (bytesLeft > BUFSIZE) ? BUFSIZE : bytesLeft;
 
-				io.Write(package, bytesSent, nextPacketSize);
+				io.Write(package, bytesSent, nextPacketSize); //write part of package with size nextpacketSize to client.
 				bytesSent += nextPacketSize;
 				bytesLeft -= nextPacketSize;
 
 			}
 			Console.WriteLine ("File send");
-			// TO DO Your own code
+
 		}
 
-
-
-		/// <summary>
-		/// The entry point of the program, where the program control starts and ends.
-		/// </summary>
-		/// <param name='args'>
-		/// The command-line arguments.
-		/// </param>
 		public static void Main (string[] args)
 		{
 			Console.WriteLine ("Server starts...");
