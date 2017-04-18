5046aed2a8e8869ad0ae113652cbf297d672ee8d
mat0pad <you@example.com>
2017-04-18 08:56:44 +0200

    start

--- LIB/lib.cs
+++ LIB/lib.cs
@@ -46,36 +46,6 @@ namespace tcp
 		}
 
 		/// <summary>
-		/// Writes the text to the server/client
-		/// </summary>
-		/// <param name='outToServer'>
-		/// Network stream for writing from the server/client.
-		/// </param>
-		/// <param name='line'>
-		/// The text to write to the server/client
-		/// </param>
-		public static void writeTextTCP(NetworkStream outToServer, String line)
-		{
-			System.Text.UTF8Encoding  encoding=new System.Text.UTF8Encoding();
-			outToServer.Write(encoding.GetBytes(line), 0, line.Length);
-			outToServer.WriteByte(0);
-		}
-
-		/// <summary>
-		/// Gets the file size from the server.
-		/// </summary>
-		/// <returns>
-		/// The filesize as a long.
-		/// </returns>
-		/// <param name='inFromServer'>
-		/// Network stream for reading from the server.
-		/// </param>
-	    public static long getFileSizeTCP(NetworkStream inFromServer)
-	    {
-	    	return long.Parse(readTextTCP(inFromServer));
-	    }
-
-		/// <summary>
 		/// Check_s the file_ exists.
 		/// </summary>
 		/// <returns>
